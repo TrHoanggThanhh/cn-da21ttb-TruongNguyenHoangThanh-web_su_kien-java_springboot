cacab397f83b61731226e914e9f11987fb304d78
TrHoanggThanhh <hoanggthanhh9988@gmail.com>
2024-10-31 10:24:42 +0700

    feat: config TimeZone for Asia/Ho_Chi_Minh

---
+++ src/main/java/com/trhthanhh/event_management/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.trhthanhh.event_management.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        return mapper;
+    }
+}
