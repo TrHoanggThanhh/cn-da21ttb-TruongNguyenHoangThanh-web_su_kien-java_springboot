77075b515b0b2122a08521bed86861f3bcee59fa
TrHoanggThanhh <hoanggthanhh9988@gmail.com>
2024-11-07 20:44:25 +0700

    feat: navbar

---
+++ front-end/frontend-event_management/src/assets/assets.js
@@ -0,0 +1,11 @@
+import food1 from './food1.webp'
+import food2 from './food2.jpg'
+import logo from './logo.png'
+import search from './search.png'
+
+export const assets = {
+    food1,
+    food2,
+    logo,
+    search
+}
